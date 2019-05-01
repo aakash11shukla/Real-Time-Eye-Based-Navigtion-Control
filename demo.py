fef8c272bcb9e8098ef2138adab813b334019406
aakash11shukla <ishuklaakash97@gmail.com>
2019-05-01 21:49:21 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,3 @@
+from keras.models import load_model
+
+model = load_model('./model/2018_12_17_22_58_35.h5')
---
+++ inference_usbCam_face.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+# pylint: disable=E1101
+
+import sys
+import time
+import numpy as np
+import tensorflow as tf
+import cv2
+import dlib
+from keras.models import load_model
+from scipy.spatial import distance as dist
+from imutils import face_utils
+
+from utils import label_map_util
+from utils import visualization_utils_color as vis_util
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = './model/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = './protos/face_label_map.pbtxt'
+
+NUM_CLASSES = 2
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+class TensoflowFaceDector(object):
+    def __init__(self, PATH_TO_CKPT):
+        """Tensorflow detector
+        """
+
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+
+        with self.detection_graph.as_default():
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            self.sess = tf.Session(graph=self.detection_graph, config=config)
+            self.windowNotSet = True
+
+
+    def run(self, image):
+        """image: bgr image
+        return (boxes, scores, classes, num_detections)
+        """
+
+        image_np = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        # Actual detection.
+        start_time = time.time()
+        (boxes, scores, classes, num_detections) = self.sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        elapsed_time = time.time() - start_time
+        print('inference time cost: {}'.format(elapsed_time))
+
+        return (boxes, scores, classes, num_detections)
+
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+# detect the face rectangle 
+def detect(img, cascade = face_cascade , minimumFeatureSize=(20, 20)):
+    if cascade.empty():
+        raise (Exception("There was a problem loading your Haar Cascade xml file."))
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=1, minSize=minimumFeatureSize)
+    
+    # if it doesn't return rectangle return array
+    # with zero lenght
+    if len(rects) == 0:
+        return []
+
+    #  convert last coord from (width,height) to (maxX, maxY)
+    rects[:, 2:] += rects[:, :2]
+
+    return rects
+
+def cropEyes(frame, boxes, bound):
+	 
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	[h, w] = bound
+
+	# keep the face region from the whole frame
+	face_rect = dlib.rectangle(left = int(boxes[0,0,1]*w), top = int(boxes[0,0,0]*h),
+								right = int(boxes[0,0,3]*w), bottom = int(boxes[0,0,2]*h))
+	
+	# determine the facial landmarks for the face region
+	shape = predictor(gray, face_rect)
+	shape = face_utils.shape_to_np(shape)
+
+	#  grab the indexes of the facial landmarks for the left and
+	#  right eye, respectively
+	(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+	(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+	# extract the left and right eye coordinates
+	leftEye = shape[lStart:lEnd]
+	rightEye = shape[rStart:rEnd]
+
+	# keep the upper and the lower limit of the eye 
+	# and compute the height 
+	l_uppery = min(leftEye[1:3,1])
+	l_lowy = max(leftEye[4:,1])
+	l_dify = abs(l_uppery - l_lowy)
+
+	# compute the width of the eye
+	lw = (leftEye[3][0] - leftEye[0][0])
+
+	# we want the image for the cnn to be (26,34)
+	# so we add the half of the difference at x and y
+	# axis from the width at height respectively left-right
+	# and up-down 
+	minxl = (leftEye[0][0] - ((34-lw)/2))
+	maxxl = (leftEye[3][0] + ((34-lw)/2)) 
+	minyl = (l_uppery - ((26-l_dify)/2))
+	maxyl = (l_lowy + ((26-l_dify)/2))
+	
+	# crop the eye rectangle from the frame
+	left_eye_rect = np.rint([minxl, minyl, maxxl, maxyl])
+	left_eye_rect = left_eye_rect.astype(int)
+	left_eye_image = gray[(left_eye_rect[1]):left_eye_rect[3], (left_eye_rect[0]):left_eye_rect[2]]
+	
+	# same as left eye at right eye
+	r_uppery = min(rightEye[1:3,1])
+	r_lowy = max(rightEye[4:,1])
+	r_dify = abs(r_uppery - r_lowy)
+	rw = (rightEye[3][0] - rightEye[0][0])
+	minxr = (rightEye[0][0]-((34-rw)/2))
+	maxxr = (rightEye[3][0] + ((34-rw)/2))
+	minyr = (r_uppery - ((26-r_dify)/2))
+	maxyr = (r_lowy + ((26-r_dify)/2))
+	right_eye_rect = np.rint([minxr, minyr, maxxr, maxyr])
+	right_eye_rect = right_eye_rect.astype(int)
+	right_eye_image = gray[right_eye_rect[1]:right_eye_rect[3], right_eye_rect[0]:right_eye_rect[2]]
+
+	# if it doesn't detect left or right eye return None
+	if 0 in left_eye_image.shape or 0 in right_eye_image.shape:
+		return None
+	# resize for the conv net
+	left_eye_image = cv2.resize(left_eye_image, (34, 26))
+	right_eye_image = cv2.resize(right_eye_image, (34, 26))
+	right_eye_image = cv2.flip(right_eye_image, 1)
+	# return left and right eye
+	return (left_eye_image, right_eye_image)
+
+# make the image to have the same format as at training 
+def cnnPreprocess(img):
+	img = img.astype('float32')
+	img /= 255
+	img = np.expand_dims(img, axis=2)
+	img = np.expand_dims(img, axis=0)
+	return img
+
+def main():
+	# open the camera,load the cnn model 
+	camera = cv2.VideoCapture(0)
+	model = load_model('blinkModel.hdf5')
+	
+	# blinks is the number of total blinks ,close_counter
+	# the counter for consecutive close predictions
+	# and mem_counter the counter of the previous loop 
+	close_counter = blinks = mem_counter= 0
+	state = ''
+	tDetector = TensoflowFaceDector(PATH_TO_CKPT)
+	while True:
+
+		ret, frame = camera.read()
+
+		[h, w] = frame.shape[:2]
+		frame = cv2.flip(frame, 1)
+		(boxes, scores, classes, num_detections) = tDetector.run(frame)
+
+		vis_util.visualize_boxes_and_labels_on_image_array(
+	        frame,
+	        np.squeeze(boxes),
+	        np.squeeze(classes).astype(np.int32),
+	        np.squeeze(scores),
+	        category_index,
+	        use_normalized_coordinates=True,
+	    	max_boxes_to_draw=1,
+	    	min_score_thresh=0.4,
+	    	line_thickness=4)
+
+		# detect eyes
+		if scores[0][0] > 0.2:
+			eyes = cropEyes(frame, boxes, [h, w])
+			if eyes is None:
+				continue
+			else:
+				left_eye,right_eye = eyes
+			
+			# average the predictions of the two eyes 
+			prediction = (model.predict(cnnPreprocess(left_eye)) + model.predict(cnnPreprocess(right_eye)))/2.0
+				
+			# blinks
+			# if the eyes are open reset the counter for close eyes
+			
+			if prediction > 0.2 :
+				state = 'open'
+				close_counter = 0
+			else:
+				state = 'close'
+				close_counter += 1
+			
+			# if the eyes are open and previousle were closed
+			# for sufficient number of frames then increcement 
+			# the total blinks
+			if state == 'open' and mem_counter > 1:
+				blinks += 1
+			# keep the counter for the next loop 
+			mem_counter = close_counter 
+
+			# draw the total number of blinks on the frame along with
+			# the state for the frame
+			cv2.putText(frame, "Blinks: {}".format(blinks), (10, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+			cv2.putText(frame, "State: {}".format(state), (300, 30),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+		
+		# show the frame
+		cv2.imshow('blinks counter', frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord('q'):
+			break
+
+	# do a little clean up
+	cv2.destroyAllWindows()
+	del(camera)
+
+if __name__ == "__main__":
+	main()
